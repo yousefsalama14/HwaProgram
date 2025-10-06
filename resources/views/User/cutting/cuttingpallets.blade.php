e79f55d9853b608d02fba91ee6c0991c569f1d7d
yousefsalama14 <yousef.salama2010@gmail.com>
2025-10-06 19:19:11 +0300

    fix: update cutting

--- resources/views/User/cutting/cuttingpallets.blade.php
+++ resources/views/User/cutting/cuttingpallets.blade.php
@@ -213,7 +213,7 @@
                                </div>
 
                            </div>
-                           <div class="row">
+                           <!-- <div class="row">
                                <div class="col-12 d-flex">
                                    <div class="btn-group ms-auto">
                                        <a type="submit" class="btn btn-danger" href="{{route('user.deleteOrderDetailes',$detailes->id)}}"
@@ -222,7 +222,7 @@
                                                class="mdi mdi-cart-plus me-1"></i> اضافة الي السلة</a> --}}
                                    </div>
                                </div>
-                           </div>
+                           </div> -->
                        </div>
                        <!--end card-body-->
                    </div>
